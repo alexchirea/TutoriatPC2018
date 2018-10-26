1ba313249ff9584c8c3a06167f6e41ea5b91ee6f
Alex Chirea <alexchirea11@gmail.com>
2018-10-27 00:09:49 +0300

    Tutoriat 1

---
+++ Tutoriat 1/2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    char v[100];
+    getchar();
+    for (i = 0; i < n; i++) {
+        v[i] = getchar();
+    }
+    for (i = 0; i < n; i++) {
+        if (i % 2 == 0) v[i] = v[i] - 32;
+        printf("%c", v[i]);
+    }
+    return 0;
+}
